11c263b8ee44c9a03a4a077d34b858db6d01dda4
Buenozw <Jvictorcastelini2006@gmail.com>
2025-11-17 14:40:51 -0300

    feat: pagina error pronta

--- gs-front/src/main.tsx
+++ gs-front/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './routes/Error';
 
 const router = createBrowserRouter([
   {
